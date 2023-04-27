5214b9011134699db6613f2e17d20924e07071e0
laxmi <lkc712628@gmail.com>
2023-04-27 12:20:54 +0100

    done register page.

---
+++ src/register.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react';
+import { auth } from './firebase';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleRegister}>
+      <h2>Register</h2>
+      <label>Email:</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
+
+export default Register;
